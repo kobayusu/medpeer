17f283ac76bd8a2c5073f9aae674fdb865c2e068
kobayusu <cb3ysk@gmail.com>
2021-03-02 03:51:59 +0900

    ideas index作成

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,6 +4,20 @@ class IdeasController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @idea = Idea.new
+    @category = Category.new
+  end
+
+  def create
+    @idea = Idea.new(idea_params)
+    if @idea.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
   private 
   def idea_params
     params.require(:idea).permit(:body).merge(category_id: categories.id)
