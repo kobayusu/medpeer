d0eb227e87f1b76b5624d06c06510742f5df931e
kobayusu <cb3ysk@gmail.com>
2021-03-02 03:53:01 +0900

    モデルテーブル編集

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  validates :name, uniqueness; true
   has_many :ideas
+  validates :name,format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }, uniqueness: true
 end
--- db/migrate/20210301120951_create_ideas.rb
+++ db/migrate/20210301120951_create_ideas.rb
@@ -1,7 +1,7 @@
 class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas, primary_key: "id", id: :bigint, auto_increment: true, default: nil, force: true do |t|
-      t.references :category, null:false
+      t.references :category, null:false, foreign_key: true
       t.text   :body, null:false
     end
   end
