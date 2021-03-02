fb3f669a4f1210a99b5d7d743700f4d8b7d38b9f
kobayusu <cb3ysk@gmail.com>
2021-03-02 10:36:58 +0900

    コントローラー編集

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -10,16 +10,23 @@ class IdeasController < ApplicationController
   end
 
   def create
-    @idea = Idea.new(idea_params)
-    if @idea.save
-      redirect_to action: :index
+    binding.pry
+    if category_id = Category.find_by(name: params[:idea][:category_id]).id
+      Idea.create(idea_params.merge(Category_id: category_id))
+      @idea = Idea.new(idea_params)
+      if @idea.valid?
+         @idea.save
+         redirect_to :index
+      else
+         render :new
+      end
     else
-      render action: :new
+
     end
   end
 
   private 
   def idea_params
-    params.require(:idea).permit(:body).merge(category_id: categories.id)
+    params.require(:idea).permit(:body)
   end
 end
